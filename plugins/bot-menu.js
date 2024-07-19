95841a4cd10a34cc444b0296fc5d2aa77052de87
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2024-07-19 17:13:25 +0530

    Update bot-menu.js

--- plugins/bot-menu.js
+++ plugins/bot-menu.js
@@ -28,11 +28,11 @@ let fechaMoment, formatDate, nombreLugar, ciudad = null; const phoneNumber = '+'
 //FAKES
 let a = {'key': {'participants': '0@s.whatsapp.net', 'fromMe': false, 'id': '3B64558B07848BD81108C1D14712018E'}, 'message': {'locationMessage': {'name': `${user}`, 'jpegThumbnail': await (await fetch(pp)).buffer(), 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'}
 const ftrol = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2023, status: 1, surface : 1, message: `${user}!`, orderTitle: `▮Menu ▸`, sellerJid: '0@s.whatsapp.net' }}}
-const fload = { key : { message: `YartexBot-MD 🌟` + `\nGataNina-Li`, thumbnail: await (await fetch(pp)).buffer(), sellerJid: '0@s.whatsapp.net' }}
+const fload = { key : { message: `Queen-Sakura` + `\nDark-Hyper`, thumbnail: await (await fetch(pp)).buffer(), sellerJid: '0@s.whatsapp.net' }}
 
-m.react('🌟')
+m.react('🧚‍♀️')
   
-let menu = `*👋 Hola, ${user}*
+let menu = `*👋 Hello, ${user}*
 
 *País*: ${nombreLugar} 
 *Ciudad*: ${ciudad}
@@ -43,7 +43,7 @@ _(Para ver el menú completo)_
 !menu2
 _(Para ver el menu audios)_`
 
-await conn.reply(m.chat, menu, a, { contextInfo: { externalAdReply: {title: '👋 ¡Hola!', body: saludo, sourceUrl: ig, thumbnail: await (await fetch(pp)).buffer() }}})
+await conn.reply(m.chat, menu, a, { contextInfo: { externalAdReply: {title: 'Hello', body: saludo, sourceUrl: ig, thumbnail: await (await fetch(pp)).buffer() }}})
 
 } catch {
 conn.reply(m.chat, `*❗️ Ocurrió un error*`, m, fake, )
